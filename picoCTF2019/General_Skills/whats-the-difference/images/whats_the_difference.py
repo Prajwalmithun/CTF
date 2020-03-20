399b1de0846b61145491155e9c5eed18d2bdf4f6
vanquisher <32311274+Prajwalmithun@users.noreply.github.com>
2020-03-19 23:14:02 -0400

    Add files via upload

---
+++ picoCTF2019/General_Skills/whats-the-difference/images/whats_the_difference.py
@@ -0,0 +1,13 @@
+with open('kitters.jpg', 'rb') as f:
+    kitters = f.read()
+
+
+with open('cattos.jpg','rb') as f:
+    cattos = f.read()
+    
+flag = ''
+for i in range(min(len(kitters),len(cattos))):
+    if kitters[i] != cattos[i]:
+        flag += cattos[i]
+
+print(flag)
